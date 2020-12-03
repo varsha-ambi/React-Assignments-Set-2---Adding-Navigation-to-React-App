e6bba912f98b2f1f99c819bbb34fd1004c969e93
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-12-03 12:23:11 +0530

    sx s

---
+++ src/components/About.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <h1>You are on the about page.</h1>;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import Location from "./LocationDisplay";
+import { Route, Link, Switch, useLocation } from "react-router-dom";
 
+function Invalid() {
+  return <div>No match</div>;
+}
 class App extends Component {
-    render() {
+  render() {
+    return (
+      <>
+        <div id="main">
+          {/* Do not remove the main div */}
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+            <Route path="/" component={Invalid} />
+          </Switch>
+          <Location />
+        </div>
+      </>
+    );
+  }
 }
 
-
 export default App;
+export const LocationDisplay = Location;
